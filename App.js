3a27237127b2437c93edf444ca8e9f4183c7cd99
James Brouckman <jbrouckman@gmail.com>
2022-10-19 11:55:59 -0400

    Post demo counter app

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Button } from 'react-native';
 
 export default function App() {
+  var [count, setCount] = useState(0)
+  
   return (
-    <View style={styles.container}>
-      <Text>
-        Cole is watching this
-      </Text>
-      <Button title='test'>Click Me</Button>
-      <StatusBar style="auto" />
+    <View style={styles.container} >
+      <Text>This is a dummy app and I change things count: {count} </Text>
+      <View style={styles.item}>
+        <Button title='Increment Count' onPress={() => setCount(count+1)} style={styles.item}/>
+      </View>
+      <View style={styles.item}>
+      <Button title='Reset Count' onPress={()=> setCount(0)}/>      
+      </View>
     </View>
   );
 }
@@ -21,4 +25,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  item: {
+    paddingBottom: 2
+  }
 });
