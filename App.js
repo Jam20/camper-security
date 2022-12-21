99293caf116ff7eca9a3894eb3bef3a06ec5ac5f
James Brouckman <jbrouckman@gmail.com>
2022-12-21 00:44:49 -0500

    UI code cleanup

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import {View, TabController, Text, Button, TextField, Assets} from 'react-native-ui-lib';
+import {View, TabController, Text} from 'react-native-ui-lib';
 import { StyleSheet } from 'react-native';
 import HomePage from './components/home-page';
 import MusicPage from './components/music-page';
@@ -32,17 +32,6 @@ export default function App() {
        <TabController.TabBar enableShadows />
       </TabController>
     </View>
-
-    // <View style={styles.container} >
-    //   <Text>This is a dummy app and I will test BLE </Text>
-    //   <Text>Connected: {connected? "true" : "false"} </Text>
-    
-    //   {status != null && <Text>Running:{status.running} Left:{status.left} Right:{status.right} Reverse {status.reverse}</Text>}
-    //   <Button label='Read Status' onPress={async ()=>{setStatus(await BluetoothController.getStatus())}}/>
-    //   <Button label='Turn Off' onPress={async ()=>{await BluetoothController.sendRequest(0,0,0,0)}}/>      
-    //   <Button label='Turn On' onPress={async ()=>{await BluetoothController.sendRequest(1,0,0,0)}}/>
-      
-    // </View>
 
   );
 }
