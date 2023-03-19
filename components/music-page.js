cb88cce5828172baa8d2e8989865054214a24c69
James Brouckman <jbrouckman@gmail.com>
2023-03-19 13:34:41 -0400

    Added more songs to app added in switching songs

--- components/music-page.js
+++ components/music-page.js
@@ -4,45 +4,101 @@ import { Button, View } from 'react-native-ui-lib';
 import { Audio } from 'expo-av';
 import FFT from 'fft.js'
 import { BluetoothController } from '../modules/BluetoothController';
+
+const songs = [
+    {
+        title : "Never Gonna Give You Up",
+        artist: "Rick Astley",
+        file: require(`../assets/rickroll.mp3`)
+    },
+    {
+        title: 'Piano Man',
+        artist: 'Billy Joel',
+        file: require('../assets/piano.mp3')
+    },
+    {
+        title: "Sweet Child 'O Mine",
+        artist: "Guns and Roses",
+        file: require('../assets/sweet.mp3')
+    },
+    {
+        title: "Bohemian Rhapsody",
+        artist: "Queen",
+        file: require('../assets/bohemian.mp3')
+    }
+
+   
+]
+
 export default function MusicPage() {
     const [sound, setSound] = useState()
+    const [songIdx, setSongIdx] = useState(0)
+    const [isPlaying, setIsPlaying] = useState(false)
+
 
     async function playSound() {
-        const { sound } = await Audio.Sound.createAsync(require('../assets/rickroll.mp3'), {shouldPlay: true})
-        setSound(sound)
-        sound.setOnAudioSampleReceived((sample)=> {
-            //console.log(sample.channels[0].frames.length)
-            const fft = new FFT(4096)
-            const out = fft.createComplexArray()
-            fft.realTransform(out, sample.channels[0].frames)
-            const chunk = (arr, size) => arr.reduce((carry, _, index, orig) => !(index % size) ? carry.concat([orig.slice(index,index+size)]) : carry, []);
-            const chunks = chunk(out,2048).map((array) => Math.max(...array)).map((val)=> val/350*8191+1024)
-            BluetoothController.sendRequest({
-                running: chunks[0],
-                left: chunks[1],
-                right: chunks[2],
-                reverse: chunks[3]
-            })
-        })
+        if(!sound) return
         await sound.playAsync()
-    }
-    async function pauseSound(){
-        console.log("pausing")
-        console.log("sound")
-        await sound.pauseAsync()
-        console.log("paused")
+        setIsPlaying(true)
+        console.log("set")
     }
 
-    useEffect(() => {
-        return sound ? ()=> {
-           sound.unloadAsync()
-        } : undefined
-    }, [sound])
+    async function pauseSound(){
+        await sound.pauseAsync()
+        setIsPlaying(false)
+    }
+
+    useEffect(()=> {
+        const createSounds = async ()=> {
+            const sounds = await Promise.all(songs.map(async (song)=> {
+                const s = (await Audio.Sound.createAsync(song.file)).sound
+                s.setOnAudioSampleReceived((sample)=> {
+                    //console.log(sample.channels[0].frames.length)
+                    const fft = new FFT(4096)
+                    const out = fft.createComplexArray()
+                    fft.realTransform(out, sample.channels[0].frames)
+                    const chunk = (arr, size) => arr.reduce((carry, _, index, orig) => !(index % size) ? carry.concat([orig.slice(index,index+size)]) : carry, []);
+                    const chunks = chunk(out,2048).map((array) => Math.max(...array)).map((val)=> val/350*8191+1024)
+                    BluetoothController.sendRequest({
+                        running: chunks[0],
+                        left: chunks[1],
+                        right: chunks[2],
+                        reverse: chunks[3]
+                    })
+                })
+                return s
+            }))
+            songs.forEach((song, idx) => {
+                song.sound = sounds[idx]
+            })
+            setSound(songs[songIdx].sound)
+
+        }
+        createSounds()
+    },[])
+
+    useEffect(()=> {
+        sound.pauseAsync()
+        if(isPlaying) setIsPlaying(false)
+        const nextSound = songs[songIdx].sound
+        if(nextSound) setSound(nextSound)
+    },[songIdx])
 
     return(
         <View style={styles.container}>
-            <Button label="Play" onPress={playSound} size={Button.sizes.large} marginB-32/>
-            <Button label="Pause" onPress={pauseSound} size={Button.sizes.large}/>
+            {
+                songs[songIdx].sound &&
+                <View flex spread center row>
+                    <Button label="Skip Left" onPress={()=> setSongIdx((songs.length+songIdx-1)%songs.length)}/>
+                    {
+                        isPlaying ? 
+                        <Button label="Pause" onPress={pauseSound} size={Button.sizes.large} marginL-16 marginR-16/> :
+                        <Button label="Play" onPress={playSound} size={Button.sizes.large} marginL-16 marginR-16/>
+                    }
+                    <Button label="Skip Right" onPress={() => setSongIdx((1+songIdx)%songs.length)}/>
+                </View>
+            }
+
         </View>
     )
 }
